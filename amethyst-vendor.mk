001ab1a6dbda3988a6bf425b4e3945f6c3ac4c87
Bluekyeet <bluekyeet@bluedmc.xyz>
2026-07-16 23:32:38 +1200

    ESIM??

--- amethyst-vendor.mk
+++ amethyst-vendor.mk
@@ -2926,6 +2926,7 @@ PRODUCT_PACKAGES += \
     uimgbamanagerlibrary \
     uimservicelibrary \
     com.android.hotwordenrollment.common.util \
+    mirilhook \
     qcrilhook \
     vendor.qti.ims.callcapability-V1.0-java \
     vendor.qti.ims.callcapabilityaidlservice-V1-java \
